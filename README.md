469aa9dc5609af7a92111b32cdd2124563362383
Shuo <shuoy@nvidia.com>
2021-07-19 15:29:52 -0400

    first minimal working example with vins basic structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 tightly-coupled-visual-inertial-leg-odometry
+
+
+This package is derived from VINS-Fusion
+
---
+++ src/test/ceres_test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by shuoy on 7/19/21.
+//
+
+#include <ceres/ceres.h>
+int main(int argc, char **argv) {
+
+    return 0;
+}
